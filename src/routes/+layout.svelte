71e9d102386a579eef6c43b756ee504112a1532c
chrisp986 <cpeters986@gmail.com>
2024-08-02 12:00:20 +0200

    Added Readme.md

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="app">
+    <nav>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+    </nav>
+    <h1>Welcome to Lauf Page!</h1>
+
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <p>Lauf-Page 2024</p>
+    </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,88 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let paceMinutes: number = 0;
+    let paceSeconds: number = 0;
+
+    function convertToKilometers(paceMinutes: number, paceSeconds: number) {
+        const milesToKM: number = 1.609344;
+        const secondsToDecimal: number = paceSeconds / 60;
+        const timeInDecimal: number = paceMinutes + secondsToDecimal;
+
+        console.log("timeDecimal", timeInDecimal / milesToKM);
+
+        const minutesKM = timeInDecimal / milesToKM;
+        console.log("remainder", minutesKM % 1);
+
+        const decimalToSeconds: number = (minutesKM % 1) * 0.6;
+        console.log("decSeconds", Math.floor(minutesKM) + decimalToSeconds);
+
+        // const m = paceMinutes / milesToKM;
+        // const fractionalMinutes = (m % 1) / milesToKM;
+        // const ms = Math.floor(m) + fractionalMinutes;
+        // const ps = paceSeconds / milesToKM / 100;
+        // console.log("ps", ms);
+        // const total = ms + ps;
+
+        return (Math.floor(minutesKM) + decimalToSeconds).toFixed(2);
+    }
+
+    // function convertToKilometers(
+    //     paceMinutes: number,
+    //     paceSeconds: number,
+    // ): string {
+    //     const milesToKilometers = 1.609344;
+
+    //     const minutesInKilometers = paceMinutes / milesToKilometers;
+    //     const fractionalMinutes = minutesInKilometers % 1;
+    //     const secondsInKilometers = fractionalMinutes / 1.609344;
+
+    //     const totalMinutes =
+    //         Math.floor(minutesInKilometers) + secondsInKilometers;
+    //     const secondsConverted = paceSeconds / 100 / milesToKilometers;
+
+    //     const totalTimeInKilometers = totalMinutes + secondsConverted;
+
+    //     return totalTimeInKilometers.toFixed(2);
+    // }
+
+    // function leadingZero(number: number) {
+    //     if (number < 10) {
+    //         let m = "0" + number;
+    //     }
+    //     return m;
+    // }
+
+    // function convertToMiles(minutes) {
+    //     return (minutes * 1.60934).toFixed(2);
+    // }
+</script>
+
+<div class="container">
+    <div class="item"></div>
+    <div class="points"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="form-item">
+        <select id="number-dd" name="number" bind:value={paceMinutes}>
+            {#each { length: 20 } as _, i}
+                <option>{i + 1}</option>
+            {/each}
+        </select>
+    </div>
+    <div class="points">:</div>
+    <div class="form-item">
+        <select id="number-dd" name="number" bind:value={paceSeconds}>
+            {#each { length: 59 } as _, i}
+                <option>{i + 1}</option>
+            {/each}
+        </select>
+    </div>
+    <div class="form-item">
+        <button>M - KM</button>
+    </div>
+    <div class="item row-3">
+        <p>Pace in Minutes/Mile: {paceMinutes}:{paceSeconds}</p>
+        <p>
+            Pace in Minutes/KM: {convertToKilometers(paceMinutes, paceSeconds)}
+        </p>
+    </div>
+</div>
