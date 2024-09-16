0e2cc753fb9da22e601aa4df68b8b1e962db5ef4
chrisp986 <cpeters986@gmail.com>
2024-09-16 14:07:54 +0200

    Design reddit

--- src/lib/components/redditPosts.svelte
+++ src/lib/components/redditPosts.svelte
@@ -57,27 +57,44 @@
   {:else}
     <ul>
       {#each redditPosts.slice(0, limitPostsVisible) as post}
-        <div
-          class="hover:bg-accent border-2 m-1 p-5 bg-white grid"
-          style="position:relative"
-        >
-          <li>
-            <a
-              href={post.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              class="font-medium text-medium md:text-lg"
+        <!-- {#if post.subreddit != "AdvancedRunning"} -->
+        <li>
+          <div
+            class="hover:bg-accent border-2 m-1 bg-white grid grid-cols-[10%_70%_20%] grid-rows-2"
+            style="position:relative"
+          >
+            <div
+              class="flex items-center justify-center row-span-2 bg-slate-300 p-3 font-medium"
             >
-              <span class="link-spanner"></span>{post.title}</a
+              {post.score}
+            </div>
+
+            <div class="max-w-2xl px-3 pt-2">
+              <a
+                href={post.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                class="font-medium text-medium md:text-lg"
+              >
+                <span class="link-spanner"></span>
+                <p class="chars-limit-header">
+                  {post.title}
+                </p></a
+              >
+            </div>
+            <div class="border-l-2 flex items-end justify-end pr-2">
+              r/{post.subreddit}
+            </div>
+
+            <div
+              class="text-xs md:text-sm chars-limit-text pl-3 pt-1 text-gray-500"
             >
-            <p class="text-sm md:text-base chars_limit_sm md:chars_limit_md">
               {post.selftext}
-            </p>
-            <p>
-              Subreddit: r/{post.subreddit} | Score: {post.score} |
-            </p>
-          </li>
-        </div>
+            </div>
+            <div class="border-l-2"></div>
+          </div>
+        </li>
+        <!-- {/if} -->
       {/each}
     </ul>
   {/if}
